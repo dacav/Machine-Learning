544a9df51cb99a80d8949fbc154370debea49c03
Giovanni [dacav] Simoni <simgidacav@gmail.com>
2011-08-25 17:16:40 +0200

    More stuff with the project
    Now I can get the genes having a threshold which produces the greatest
    information gain.
    
    Unfortunately something seems to be wrong, since the maximal information
    gain shown is 0. This probably means that the separation splitted the
    examples into two sets: one of them is empty, the other is full of all
    instances. This is totally weird.
    
    I guess there's some bug into the code, I'd better check it out.

---
+++ src/const.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+HIGHEST_TO_KEEP = 100
+
--- src/logic.py
+++ src/logic.py
@@ -9,11 +9,12 @@
 #
 
 from __future__ import print_function, division
-import sys
 import itertools as it
 import math
+import sys
+from operator import itemgetter
 
-import utils
+import utils, const
 
 try: range = xrange
 except: pass
@@ -43,27 +44,37 @@ def threshold (xs_byfeat, classes, get_val, get_class):
         - The classes manager;
         - The callbacks to extract value and class from an example
     Output:
-        The threshold value which, if used wrt the selected feature,
-        maximizes the information gain.
+        A tuple containing:
+        - The threshold value which, if used wrt the selected feature,
+          maximizes the information gain;
+        - The value of the information gain itself.
     '''
     classes_of = lambda xs : it.imap( get_class, xs )
     H = lambda xs : entropy(classes.count_by(classes_of(xs)).itervalues())
-
     xs_entropy = H(xs_byfeat)
+
     def IG (thr):
-        predicate = lambda x : x < thr
-        h0 = H( it.ifilter(predicate, xs_byfeat) ) 
+        predicate = lambda x : x[0] < thr
+        h0 = H( it.ifilter(predicate, xs_byfeat) )
         h1 = H( it.ifilterfalse(predicate, xs_byfeat) )
         return (thr, xs_entropy - h0 - h1)
 
     cts = candidate_thresholds(xs_byfeat, classes, get_val, get_class)
-    return max( it.imap(IG, cts), key=lambda x : x[1] )[0]
+    return max( it.imap(IG, cts), key=itemgetter(1) )
 
 def run (problem):
     examples = problem.get_examples()
     classes = problem.get_classes()
-    for i in range(problem.nfeats()):
-        thr = threshold(examples.sort_by(i), classes, 
-                        examples.get_val, examples.get_class)
-        print("Working with examples for gene", i, ":", thr)
-    
+    compute_threshold = lambda gen_id : threshold(
+        utils.ensure_list(examples.sort_by(gen_id)),
+        classes,
+        examples.get_val,
+        examples.get_class
+    )
+    genes = sorted(
+        ((i, compute_threshold(i)) for i in range(problem.nfeats())),
+        key=lambda (gen_id, (thrs, igain)) : igain,
+        reverse=True
+    )
+    return genes
+
